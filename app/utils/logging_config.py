ebaf1f0f8ed211f904c6ef3a30bc987dd32ba51c
GoodWeather0322 <a82521112@gmail.com>
2024-10-15 09:18:04 +0800

    ✨ feat: 新增統一logging_config

---
+++ app/utils/logging_config.py
@@ -0,0 +1,9 @@
+import logging
+
+LOGGING_LEVEL = logging.DEBUG
+
+logging.basicConfig(
+    level=LOGGING_LEVEL, format="[%(levelname)s] %(asctime)s %(name)s : %(message)s"
+)
+
+logger = logging.getLogger(__name__)
