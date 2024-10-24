1eb15b150458ed1a7136085d2f2f539abf5ccf0b
GoodWeather0322 <a82521112@gmail.com>
2024-10-24 10:26:24 +0800

    ✨ feat(fastapi-lifespan): fastapi lifespan新增appscheduler，可以在fastapi服務啟動時啟動排程，結束時關閉，隨時可加入任務

--- app/main.py
+++ app/main.py
@@ -2,10 +2,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import uvicorn
-from app.routers import router
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="fastapi-bot")
+from app.routers import router
+from app.utils.config import settings
+from app.services.scheduler import scheduler
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 啟動排程器
+    scheduler.start()
+
+    yield
+
+    # 關閉排程器
+    scheduler.shutdown()
+
+
+app = FastAPI(title="fastapi-bot", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,4 +31,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router.router, prefix="/routers")
+app.include_router(router.router, prefix="/api")
